bf0b69ab2df01db2e0edbfbc05f78b27cbc835a4
Hectows <51876843+Hectows@users.noreply.github.com>
2022-01-08 00:17:01 +0300

    experience ilerlendi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,15 +5,24 @@ function openTab(evt, companyName){
     for(i = 0; i < tabcontent.length; i++){
         tabcontent[i].style.display = "none";
     }
-    tabLinks = document.getElementsByClassName("experienceTabLinks");
-    for(i = 0; i < tabLinks.length; i++){
-        tabLinks[i].className = tabLinks[i].className.replace("active", "");
-    }
+    tabLinks = document.getElementsByClassName('experienceTabLinks');
 
+    for(i = 0; i < tabLinks.length; i++){
+        tabLinks[i].style.backgroundColor = "#b55400";
+        tabLinks[i].className = tabLinks[i].className.replace("active", "");    
+    }
+    
     document.getElementById(companyName).style.display = "block";
+    document.getElementById(getCompanyButtonName(companyName)).style.backgroundColor = "red";
+   // evt.currentTarget.style.backgroundColor = "red";
     evt.currentTarget.className += "active";
+    
 }
 
 function clickOnDefaultTab(){
-    document.getElementById("defaultOpen").click();
+    document.getElementById("provenButton").click();
+}
+
+function getCompanyButtonName(companyName){
+    return companyName.charAt(0).toLowerCase() + companyName.slice(1) + "Button";
 }
